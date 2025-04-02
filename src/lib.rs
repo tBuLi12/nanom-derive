5b4ad42b88bf83adfdb69bafb5ea9a978e0ddb1b
Jeremi Sobierski <tbuli1112@gmail.com>
2025-04-02 17:31:12 +0200

    adds InfoJs for references

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,46 @@
+use std::iter;
+
 use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
-use syn::{ext::IdentExt, Data, DataEnum, DataStruct, Fields};
-use synstructure::{quote, BindStyle, Structure};
+use proc_macro2::{Group, Punct, TokenTree};
+use syn::{ext::IdentExt, Data, DataEnum, DataStruct, Fields, Token};
+use synstructure::{quote, BindStyle, MacroResult, Structure};
 
-fn derive_js_object(mut input: Structure) -> TokenStream {
-    for ty in input.referenced_ty_params() {
-        input.add_where_predicate(syn::parse_quote! { #ty: ::nanom::IntoJs + ::nanom::FromJs });
+fn ts_type_body(input: &Structure) -> proc_macro2::TokenStream {
+    let type_name = input.ast().ident.unraw().to_string();
+
+    match &input.ast().data {
+        Data::Union(_) => panic!("Unions cannot be used to derive JsObject"),
+        Data::Struct(DataStruct { fields, .. }) => {
+            let fields = fields_ts_type(&fields);
+            quote! { ::nanom::typing::Type::NamedObject { name: #type_name.to_string(), fields: #fields, id: ::nanom::typing::TypeRef { get_type: <Self as ::nanom::TsType>::ts_type } } }
+        }
+        Data::Enum(DataEnum { variants, .. }) => {
+            let add_variants = variants.into_iter().map(|variant| {
+                let name_str = variant.ident.unraw().to_string();
+
+                let fields_type = fields_ts_type(&variant.fields);
+
+                quote! {
+                    {
+                        enum_fields.insert(#name_str.to_string(), #fields_type); 
+                    }
+                }
+            });
+
+            quote! {
+                {
+                    let mut enum_fields = ::std::collections::HashMap::new();
+                    #(#add_variants)*
+                    ::nanom::typing::Type::Enum { kinds: enum_fields, name: #type_name.to_string(), id: ::nanom::typing::TypeRef { get_type: <Self as ::nanom::TsType>::ts_type } }
+                }
+            }
+        }
     }
+}
+
+fn into_js_body(input: &mut Structure) -> proc_macro2::TokenStream {
+    input.bind_with(|_| BindStyle::Ref);
 
     let kind = input.each_variant(|variant| {
         let name = variant.ast().ident.unraw().to_string();
@@ -40,7 +74,7 @@ fn derive_js_object(mut input: Structure) -> TokenStream {
         }
     };
 
-    let to_js = match input.ast().data {
+     match input.ast().data {
         Data::Union(_) => panic!("Unions cannot be used to derive JsObject"),
         Data::Struct(_) => get_object,
         Data::Enum(_) => quote! {
@@ -60,9 +94,11 @@ fn derive_js_object(mut input: Structure) -> TokenStream {
                 object
             }
         },
-    };
+    }
+}
 
-    let from_js = match &input.ast().data {
+fn from_js_body(input: &Structure) -> proc_macro2::TokenStream {
+    match &input.ast().data {
         Data::Union(_) => panic!("Unions cannot be used to derive JsObject"),
         Data::Struct(DataStruct { fields, .. }) => {
             let parse_fields = parse_fields(&fields);
@@ -104,42 +140,70 @@ fn derive_js_object(mut input: Structure) -> TokenStream {
                 }
             }
         }
-    };
+    }
+}
 
-    let type_name = input.ast().ident.unraw().to_string();
+fn derive_ts_type(mut input: Structure) -> TokenStream {
+    for ty in input.referenced_ty_params() {
+        input.add_where_predicate(syn::parse_quote! { #ty: ::nanom::TsType });
+    }
 
-    let ts_type = match &input.ast().data {
-        Data::Union(_) => panic!("Unions cannot be used to derive JsObject"),
-        Data::Struct(DataStruct { fields, .. }) => {
-            let fields = fields_ts_type(&fields);
-            quote! { ::nanom::typing::Type::NamedObject { name: #type_name.to_string(), fields: #fields, id: ::std::any::TypeId::of::<Self>() } }
-        }
-        Data::Enum(DataEnum { variants, .. }) => {
-            let add_variants = variants.into_iter().map(|variant| {
-                let name_str = variant.ident.unraw().to_string();
-
-                let fields_type = fields_ts_type(&variant.fields);
-
-                quote! {
-                    {
-                        enum_fields.insert(#name_str.to_string(), #fields_type); 
-                    }
-                }
-            });
-
-            quote! {
-                {
-                    let mut enum_fields = ::std::collections::HashMap::new();
-                    #(#add_variants)*
-                    ::nanom::typing::Type::Enum { kinds: enum_fields, name: #type_name.to_string(), id: ::std::any::TypeId::of::<Self>() }
-                }
-            }
-        }
-    };
+    let ts_type = ts_type_body(&input);
 
     input
-        .unbound_impl(quote!(::nanom::JsObject), quote! {
+        .unbound_impl(quote!(::nanom::TsType), quote! {
+            fn ts_type() -> ::nanom::typing::Type {
+                #ts_type
+            }
+        })
+        .into()
+}
+
+fn derive_into_js(mut input: Structure) -> TokenStream {
+    for ty in input.referenced_ty_params() {
+        input.add_where_predicate(syn::parse_quote! { #ty: ::nanom::IntoJs });
+    }
+
+    let to_js = into_js_body(&mut input);
+
+    input
+        .unbound_impl(quote!(::nanom::IntoJs), quote! {
             fn into_js(self, env: ::nanom::napi::Env) -> ::std::result::Result<::nanom::napi::Value, ::nanom::ConversionError> {
+                ::std::result::Result::Ok(unsafe #to_js)
+            }
+        })
+        .into()
+}
+
+fn derive_from_js(mut input: Structure) -> TokenStream {
+    for ty in input.referenced_ty_params() {
+        input.add_where_predicate(syn::parse_quote! { #ty: ::nanom::FromJs });
+    }
+
+    let from_js = from_js_body(&input);
+
+    input
+        .unbound_impl(quote!(::nanom::FromJs), quote! {
+            fn from_js(env: ::nanom::napi::Env, value: ::nanom::napi::Value) -> ::std::result::Result<Self, ::nanom::ConversionError> {
+                ::std::result::Result::Ok(unsafe #from_js)
+            }
+        })
+        .into()
+}
+
+fn derive_js_object(mut input: Structure) -> TokenStream {
+    for ty in input.referenced_ty_params() {
+        input.add_where_predicate(syn::parse_quote! { #ty: ::nanom::FromJs + ::nanom::IntoJs });
+        input.add_where_predicate(syn::parse_quote! { for<'a> &'a #ty: ::nanom::IntoJs }); 
+    }
+
+    let to_js = into_js_body(&mut input);
+    let from_js = from_js_body(&input);
+    let ts_type = ts_type_body(&input);
+
+    let tokens = input
+        .unbound_impl(quote!(::nanom::JsObject), quote! {
+            fn into_js(&self, env: ::nanom::napi::Env) -> ::std::result::Result<::nanom::napi::Value, ::nanom::ConversionError> {
                 ::std::result::Result::Ok(unsafe #to_js)
             }
             
@@ -150,8 +214,53 @@ fn derive_js_object(mut input: Structure) -> TokenStream {
             fn ts_type() -> ::nanom::typing::Type {
                 #ts_type
             }
-        })
-        .into()
+        });
+
+    let tokens2 = input
+        .unbound_impl(quote!(::nanom::IntoJs), quote! {
+            fn into_js(self, env: ::nanom::napi::Env) -> ::std::result::Result<::nanom::napi::Value, ::nanom::ConversionError> {
+                ::std::result::Result::Ok(unsafe #to_js)
+            }
+        });
+    
+        if tokens2.is_empty() {panic!("")}
+
+    let tokens3 = input
+        .unbound_impl(quote!(::nanom::TsType), quote! {
+            fn ts_type() -> ::nanom::typing::Type {
+                #ts_type
+            }
+        });
+
+    tokens.into_stream().into_iter().chain(add_at_before_for(tokens2).into_stream().into_iter()).chain(add_at_before_for(tokens3).into_stream().into_iter()).collect()
+}
+
+fn add_at_before_for(mut tokens: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+    fn add_at_before_for_inner(mut tokens: proc_macro2::TokenStream, first: &mut bool) -> proc_macro2::TokenStream {
+        let mut tokens = tokens.into_iter();
+        let mut result: Vec<TokenTree> = vec![];
+    
+        for token in tokens {
+            match &token {
+                TokenTree::Ident(ident) if ident.to_string() == "for" && *first => {
+                    println!("inserting for");
+                    result.push(token);
+                    result.push(TokenTree::Punct(Punct::new('&', proc_macro2::Spacing::Alone)));
+                    *first = false;
+                }
+                TokenTree::Group(group) if *first => {
+                    result.push(TokenTree::Group(Group::new(group.delimiter(), add_at_before_for_inner(group.stream(), first))));
+                }
+                _ => {
+                    println!("not inserting for: {}", token);
+                    result.push(token);
+                }
+            }
+        }
+        result.into_iter().collect()
+    }
+
+    add_at_before_for_inner(tokens, &mut true)
 }
 
 fn parse_fields(fields: &Fields) -> proc_macro2::TokenStream {
@@ -176,7 +285,7 @@ fn parse_fields(fields: &Fields) -> proc_macro2::TokenStream {
     }
 }
 
-fn fields_ts_type(fields: &Fields) -> proc_macro2::TokenStream {
+fn  fields_ts_type(fields: &Fields) -> proc_macro2::TokenStream {
     let Fields::Named(fields) = fields else {
         panic!("Only named structs can be used to derive JsObject");
     };
@@ -199,4 +308,7 @@ fn fields_ts_type(fields: &Fields) -> proc_macro2::TokenStream {
     }
 }
 
+synstructure::decl_derive!([TsType] => derive_ts_type);
+synstructure::decl_derive!([FromJs] => derive_from_js);
+synstructure::decl_derive!([IntoJs] => derive_into_js);
 synstructure::decl_derive!([JsObject] => derive_js_object);
